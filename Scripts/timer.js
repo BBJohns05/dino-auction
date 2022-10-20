3d2c6bac87d7f87b4f9c05bde4f1886c4578c658
dakotarobot213 <110577432+dakotarobot213@users.noreply.github.com>
2022-10-20 15:18:49 -0700

    Improved text readability and organized HTML enhancing elments
    Added a black transparent background to small text to make it easier on the eyes to read

---
+++ Scripts/timer.js
@@ -0,0 +1,28 @@
+// The date we're counting down to
+var count = new Date('Dec 12, 2022 15:37:25').getTime();
+
+// Updates the count down every second
+var x = setInterval(function () {
+	// Get today's date and time
+	var now = new Date().getTime();
+
+	// Calculates the duration between now and the specified date
+	var distance = count - now;
+
+	// Time calculations for days, hours, minutes and seconds
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+	// Display the result in the element with id="demo"
+	document.getElementById(
+		'timer'
+	).innerHTML = `Time left for auction: ${days} days, ${hours} hours, and ${minutes} minutes `;
+
+	// If the count down is finished, replace the time with an alert
+	if (distance < 0) {
+		clearInterval(x);
+		document.getElementById('timer').innerHTML = 'Sold!';
+	}
+}, 1000);
